a051fb3f851584e3e2276e6803d1ba3e93167a25
Jeremiahl <huliang2024@outlook.com>
2024-01-25 10:41:58 +0800

    Structure

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Definitions.h>
+#define INITIALVALUE 0
+
+double pi = 3.14;
+
+int main()
+{
+    char deviceName[] = "EPOS4";
+    char protocolStackName[] = "MAXON SERIAL V2";
+    char portName[]= "USB0";
+    char interfaceName[] = "USB";
+    DWORD errorCode = 0;
+    HANDLE keyHandle = 0;
+    BOOL status = INITIALVALUE;
+    int FLAG = 0;
+    bool s = TRUE;
+    std::string book = "What the Fuck are you doing?";
+    // 打开设备
+    keyHandle = VCS_OpenDevice(deviceName, protocolStackName, interfaceName, portName, &errorCode);
+    if (!keyHandle) std::cout << "No USB Devices Detected\n";
+    else { std::cout << "Connected Successfully\n"; FLAG++; }
+
+    // 配置电机、传感器以及编码器的设备参数
+    WORD motorType = MT_DC_MOTOR;
+    WORD nodeID = 1;
+    status = VCS_SetMotorType(keyHandle, nodeID, motorType, &errorCode);
+    if (!status) std::cout << "Motor Set Failed\n";
+    else { std::cout << "SET Successfully"; FLAG++; }
+    // encoder
+    status = VCS_SetSensorType(keyHandle, nodeID, ST_INC_ENCODER2_3CHANNEL, &errorCode);
+    BOOL encoderStatus = INITIALVALUE;
+    DWORD encoderRevolution = 256;
+    encoderStatus = VCS_SetIncEncoderParameter(keyHandle, nodeID, encoderRevolution, 0, &errorCode);
+    // sensor 
+    BOOL sensorStatus = INITIALVALUE;
+    WORD sensorType;
+    sensorStatus = VCS_GetSensorType(keyHandle, nodeID, &sensorType, &errorCode);
+    // sensorStatus = VCS_SetHallSensorParameter();
+    // 配置模式及其参数（configuration with PPM）
+    BOOL modeStatus = INITIALVALUE;
+    modeStatus = VCS_SetOperationMode(keyHandle, nodeID, OMD_PROFILE_POSITION_MODE, &errorCode);
+    
+
+    // 配置状态机器控制电机（operation）
+
+
+    // 关闭设备
+    VCS_CloseDevice(keyHandle, &errorCode);
+
+    
+}
